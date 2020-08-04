469df66e93439c16da8afadb8adcdb8a7d17351d
dselarka <dselarka@iu.edu>
2020-08-03 20:15:53 -0400

    adding googlelogin dependency

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import './App.css';
 import GoogleLogin from 'react-google-login';
 
 function App() {
+
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <h1>Google Login</h1>
       </header>
+      <div>
+            <GoogleLogin
+              clientId="1092112084655-runh1vei6rcre98qp6n21ruv3baaif9j.apps.googleusercontent.com"
+              buttonText="Login"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={'single_host_origin'}
+            />
+        </div>
     </div>
   );
 }

86cb1caabfb1efbc2c572648e745da4d700f34ea
dselarka <dselarka@iu.edu>
2020-08-03 20:10:38 -0400

    Edit app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from 'react-google-login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Google Login</h1>
       </header>
     </div>
   );

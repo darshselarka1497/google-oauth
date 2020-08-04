ec015420e3878f0c807329f9de37a96a6b40cb9e
dselarka <dselarka@iu.edu>
2020-08-03 20:31:06 -0400

    Final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GoogleLogin from 'react-google-login';
 
 function App() {
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [url, setUrl] = useState("");
+
+
   const responseGoogle = (response) => {
     console.log(response);
+    setName(response.profileObj.name);
+    setEmail(response.profileObj.email);
+    setUrl(response.profileObj.imageUrl);
   }
-  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -22,6 +30,11 @@ function App() {
               onFailure={responseGoogle}
               cookiePolicy={'single_host_origin'}
             />
+        </div>
+        <div>
+          <h2>Welcome {name}</h2>
+          <h2>Email: {email}</h2>
+          <img src={url} alt={name}/>
         </div>
     </div>
   );
